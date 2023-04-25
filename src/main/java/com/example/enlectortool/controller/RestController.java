60ef8e2137b557d76022acbc1fc2cdfb8b0665c9
Martin Blazek <martinblazek6@gmail.com>
2023-04-25 20:43:36 +0200

    confirm email redirection done

--- src/main/java/com/example/enlectortool/controller/RestController.java
+++ src/main/java/com/example/enlectortool/controller/RestController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class RestController {
     private final EmailVerificationService emailVerificationService;
     private final StudentRepository studentRepository;
+    private final String SERVER = System.getenv("SERVER");
 
 
     @GetMapping("/login/{id}/{hashcode}")
@@ -27,15 +28,11 @@ public class RestController {
             studentRepository.saveAndFlush(user);
             return ResponseEntity.ok("<body style=\"background-color: #f2f2f2; font-family: Arial, sans-serif; margin: 0; padding: 0; text-align: center;\">\n" +
                     "\t<h1 style=\"color: #0077cc; margin-top: 50px;\">Welcome to english class inc!</h1>\n" +
-                    "\t<p style=\"font-size: 18px; margin-top: 20px;\">We are happy to see you "+user.getFirstName()+". With this Id you can assign to lection "+user.getStudentId()+"</p>\n" +
-                    "\t<button style=\"background-color: #0077cc; border: none; border-radius: 5px; color: #fff; font-size: 16px; padding: 10px 20px; margin-top: 20px; cursor: pointer;\" onmouseover=\"this.style.backgroundColor='#0066b3'\" onmouseout=\"this.style.backgroundColor='#0077cc'\" onclick=\"redirectToPage\">Go to page</button>\n" +
-                    "\n" +
-                    "\t<script>\n" +
-                    "\t\tfunction redirectToPage() {\n" +
-                    "\t\t\twindow.location.href = \"http://localhost:8080\";\n" +
-                    "\t\t}\n" +
-                    "\t</script>\n" +
+                    "\t<p style=\"font-size: 18px; margin-top: 20px;\">We are happy to see you "+user.getFirstName()+". With this Id you can assign to lection, Your ID:  "+user.getStudentId()+"</p>\n" +
+                    "\t<a href=\""+SERVER+"\" target=\"_blank\" style=\"background-color: #0077cc; border: none; border-radius: 5px; color: #fff; font-size: 16px; padding: 10px 20px; margin-top: 20px; cursor: pointer; text-decoration: none;\" onmouseover=\"this.style.backgroundColor='#0066b3'\" onmouseout=\"this.style.backgroundColor='#0077cc'\">Go to page</a>\n" +
                     "</body>");
+
+
         }
         return ResponseEntity.status(400).body("Something went wrong");
     }
